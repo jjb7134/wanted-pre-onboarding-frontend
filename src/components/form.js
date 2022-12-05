4dd84ef529dcf5bcaf43941b233ac03881194237
firely95 <90025832+jjb7134@users.noreply.github.com>
2022-12-06 01:14:52 +0900

    login form 수정

--- src/components/form.js
+++ src/components/form.js
@@ -1,18 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classes from './form.module.css'
 
 function Form() {
-  return (
-    <div>
-        <form className={classes.formWrap}>
-            <p>이메일</p>
-            <label typeof='input' className={classes.inputEmail}></label>
-            <p>비밀번호</p>
-            <label typeof='input' className={classes.inputPass}></label>
-            <label typeof='submit' className={classes.btn}>로그인</label>
-        </form>
-    </div>
-  )
-}
+  
+    const [values, setValues] = useState({
+      email: "",
+      password: "",
+    })
+  
+    const handleChange = e => {
+      setValues({
+        ...values,
+        [e.target.name]: e.target.value,
+      })
+    }
+  
+    const handleSubmit = e => {
+      e.preventDefault()
+    }
+  
+    return (
+      <form className={classes.formWrap} onSubmit={handleSubmit}>
+        <div>
+        <label className={classes.mail}>이메일</label><br/>
+        <input
+          className={classes.emailinput}
+          type="text"
+          name="email"
+          value={values.email}
+          onChange={handleChange}
+        />
+        </div>
+        <div>
+          <label className={classes.pass}>비밀번호</label><br/>
+          <input
+          className={classes.passinput}
+            type="password"
+            name="password"
+            value={values.password}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+        <button className={classes.btn} type="submit">로그인</button>
+        </div>
+      </form>
+    )
+  }
 
 export default Form;
