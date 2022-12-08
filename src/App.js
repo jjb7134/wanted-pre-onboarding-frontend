3c7e8bbe20c869d184131ee5421174e2b58fc391
firely95 <90025832+jjb7134@users.noreply.github.com>
2022-12-09 01:24:08 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import {Route,Routes} from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 
-import Sign from './components/sign';
-import Welcome from './components/root/welcome';
-import Form from './components/root/form';
-import Join from './components/root/join';
-import Todolist from './components/todo/Todolist';
-
+import Sign from "./components/root/sign";
+import Welcome from "./components/root/welcome";
+import Form from "./components/root/form";
+import Join from "./components/root/join";
+import Todolist from "./components/todo/Todolist";
 
 function App() {
   return (
     <div>
-      
       {/* 여러 페이지 라우트 처리 */}
       <Routes>
         {/* /경로 */}
-        <Route path='/' exact element={ <div><Welcome /> <Form/> <Sign /></div>} />
-        <Route path='/join' element={ <div><Welcome /> <Join/> <Sign /></div>} />
-        <Route path='/todo' element={ <Todolist/>} />
+        <Route
+          path="/"
+          exact
+          element={
+            <div>
+              <Welcome /> <Form /> <Sign />
+            </div>
+          }
+        />
+        <Route
+          path="/join"
+          element={
+            <div>
+              <Welcome /> <Join /> <Sign />
+            </div>
+          }
+        />
+        <Route path="/todo" element={<Todolist />} />
       </Routes>
-      
     </div>
   );
 }
